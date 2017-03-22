bedb34cb7e32b8454ccc1c6fd2d3332ef2d128f6
Dario Campagna <campagna@esteco.com>
2017-03-22 12:45:35 +0100

    Replaced label with table.

--- src/main/java/it/esteco/auction/sniper/MainWindow.java
+++ src/main/java/it/esteco/auction/sniper/MainWindow.java
@@ -2,36 +2,68 @@ package it.esteco.auction.sniper;
 
 import javax.swing.*;
 import javax.swing.border.LineBorder;
+import javax.swing.table.AbstractTableModel;
 import java.awt.*;
 
 public class MainWindow extends JFrame {
 
     public static final String MAIN_WINDOW_NAME = "Auction Sniper Main";
-    public static final String SNIPER_STATUS_NAME = "sniper status";
+    private static final String SNIPERS_TABLE_NAME = "Snipers Table";
     public static final String STATUS_JOINING = "Joining";
     public static final String STATUS_BIDDING = "Bidding";
     public static final String STATUS_LOST = "Lost";
     public static final String STATUS_WINNING = "Winning";
     public static final String STATUS_WON = "Won";
 
-    private final JLabel sniperStatus = createLabel(STATUS_JOINING);
+    private final SnipersTableModel snipers = new SnipersTableModel();
 
     public MainWindow() {
         super("Auction Sniper");
         setName(MAIN_WINDOW_NAME);
-        add(sniperStatus);
+        fillContentPane(makeSnipersTable());
+        pack();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
 
-    public void showStatus(String status) {
-        sniperStatus.setText(status);
+    public void showStatus(String statusText) {
+        snipers.setStatusText(statusText);
     }
 
-    private JLabel createLabel(String initialText) {
-        JLabel result = new JLabel(initialText);
-        result.setName(SNIPER_STATUS_NAME);
-        result.setBorder(new LineBorder(Color.black));
-        return result;
+    private void fillContentPane(JTable snipersTable) {
+        final Container contentPane = getContentPane();
+        contentPane.setLayout(new BorderLayout());
+        contentPane.add(new JScrollPane(snipersTable), BorderLayout.CENTER);
+    }
+
+    private JTable makeSnipersTable() {
+        final JTable snipersTable = new JTable(snipers);
+        snipersTable.setName(SNIPERS_TABLE_NAME);
+        return snipersTable;
+    }
+
+    public class SnipersTableModel extends AbstractTableModel {
+
+        private String statusText = STATUS_JOINING;
+
+        @Override
+        public int getRowCount() {
+            return 1;
+        }
+
+        @Override
+        public int getColumnCount() {
+            return 1;
+        }
+
+        @Override
+        public Object getValueAt(int rowIndex, int columnIndex) {
+            return statusText;
+        }
+
+        public void setStatusText(String newStatusText) {
+            statusText = newStatusText;
+            fireTableRowsUpdated(0, 0);
+        }
     }
 }
--- src/test/java/test/auctionsniper/e2e/runner/AuctionSniperDriver.java
+++ src/test/java/test/auctionsniper/e2e/runner/AuctionSniperDriver.java
@@ -2,11 +2,11 @@ package test.auctionsniper.e2e.runner;
 
 import com.objogate.wl.swing.AWTEventQueueProber;
 import com.objogate.wl.swing.driver.JFrameDriver;
-import com.objogate.wl.swing.driver.JLabelDriver;
+import com.objogate.wl.swing.driver.JTableDriver;
 import com.objogate.wl.swing.gesture.GesturePerformer;
 
+import static com.objogate.wl.swing.matcher.JLabelTextMatcher.withLabelText;
 import static it.esteco.auction.sniper.MainWindow.MAIN_WINDOW_NAME;
-import static it.esteco.auction.sniper.MainWindow.SNIPER_STATUS_NAME;
 import static org.hamcrest.CoreMatchers.equalTo;
 
 public class AuctionSniperDriver extends JFrameDriver {
@@ -22,6 +22,6 @@ public class AuctionSniperDriver extends JFrameDriver {
     }
 
     public void showsSniperStatus(String statusText) {
-        new JLabelDriver(this, named(SNIPER_STATUS_NAME)).hasText(equalTo(statusText));
+        new JTableDriver(this).hasCell(withLabelText(equalTo(statusText)));
     }
 }
